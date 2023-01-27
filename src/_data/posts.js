70dd21228193d8ddfe349146c1b3386be15e5736
Morten Zwarenstein <mortenzwarenstein@hotmail.com>
2023-01-27 17:50:40 +0100

    added blogpost functionality

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -3,4 +3,7 @@ const {Directus} = require('@directus/sdk');
 let client = new Directus('http://localhost:8055');
 
 let items = client.items('posts').readByQuery({ sort: ['-date_created'] });
-module.exports = async () => (await items)?.data;
+module.exports = async () => {
+    let data = (await items)?.data;
+    return data
+}
