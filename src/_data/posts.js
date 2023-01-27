9874da20f5f79841103f0b9d3c690356d597e153
Morten Zwarenstein <mortenzwarenstein@hotmail.com>
2023-01-27 23:38:44 +0100

    set env

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,6 +1,7 @@
 const {Directus} = require('@directus/sdk');
 
-let client = new Directus('http://localhost:8055');
+
+let client = new Directus(process.env.DIRECTUS_URL);
 
 let items = client.items('posts').readByQuery({ sort: ['-date_created'] });
 module.exports = async () => {
